3e731f4c12e4ec20e9e3333b21f8503b81e4c111
Krijor08 <krijor08@gmail.com>
2026-05-31 21:54:35 +0200

    move handlers to main.go

--- main.go
+++ main.go
@@ -2,24 +2,37 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 
-	"example.com/go/backend/handlers"
 	"example.com/go/backend/middleware"
 
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
+var templates = template.Must(
+	template.ParseGlob("templates/*.html"),
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "base.html", "home.html")
+
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "base.html", "about.html")
+}
+
 func main() {
 	godotenv.Load()
 
 	r := mux.NewRouter()
 
-	homeHandler := http.HandlerFunc(handlers.Home)
-	aboutHandler := http.HandlerFunc(handlers.About)
+	homeHandler := http.HandlerFunc(Home)
+	aboutHandler := http.HandlerFunc(About)
 
 	r.HandleFunc("/", homeHandler.ServeHTTP).Methods("GET")
 	r.HandleFunc("/about", aboutHandler.ServeHTTP).Methods("GET")
