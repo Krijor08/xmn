0c5eefed9c8bd4fb0b5e36899fb6f2e8a0dc2eaa
Krijor08 <krijor08@gmail.com>
2026-06-11 00:05:02 +0200

    Added INSERT statements file for testing etc

---
+++ sql/values.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (username, password, email, phone, role_ID)
+VALUES 
+('user', '1234', 'user@test.com', 12345678, 1002),
+('test', 'test', NULL, NULL, NULL);
+
+INSERT INTO roles (role)
+VALUES ('guest'), ('admin'), ('user');
